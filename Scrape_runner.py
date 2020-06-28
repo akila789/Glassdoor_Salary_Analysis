b4ebeed18181f9e229c2fdfbe3e77f6d6b185e1b
akila789 <akila789@gmail.com>
2020-06-28 18:26:08 +0530

    Uploading data modification to runner

--- Scrape_runner.py
+++ Scrape_runner.py
@@ -3,6 +3,6 @@ import pandas as pd
 
 path = "D://Source//Glassdoor_Salary_Analysis//chromedriver"
 
-df = s.get_jobs('data scientist', 15, False, path, 15)
+df = s.get_jobs('data scientist', 1000, False, path, 2)
 
-df.to_csv('Results.csv')
+df.to_csv('Glassdoor_jobs.csv')
